fff33f3029006088176d964fefacd4b219aa3a0c
Asthere0n <cristiansanchezsonido@gmail.com>
2025-04-08 00:53:57 +0100

    Product scroll is sorted in alphabetical order, to ease the UX of searching.

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -13,22 +13,29 @@ import TeaIcon from "./svg/TeaIcon";
 
 
 function Accordion(props) {
-    let productAccordion = productsList.map(product => (
+    let productAccordion = productsList.sort((a, b) => a.name.localeCompare(b.name));
+    productAccordion = productAccordion.map(product => (
         <ProductButton key={product.id} product={product} onProductSelect={props.onProductSelect} />
     ));
 
+    const productFiltering = (category) => {
+        const filteredList = productsList.filter((item) => item.category === category)   
+        console.log(category.toUpperCase())         
+        filteredList.map(item=>console.log(item.name))
+    }
+
     return (<ul id="itemList">
         <div id="filter-list">
-            <button className="filter cake"><CakeIcon /></button>
-            <button className="filter chilled"><ChilledIcon /></button>
-            <button className="filter coffee"><CoffeeIcon /></button>
-            <button className="filter dairy"><DairyIcon /></button>
-            <button className="filter misc"><MiscIcon /></button>
-            <button className="filter pastry"><PastryIcon /></button>
-            <button className="filter powder"><PowderIcon /></button>
-            <button className="filter sauce"><SauceIcon /></button>
-            <button className="filter syrup"><SyrupIcon /></button>
-            <button className="filter tea"><TeaIcon /></button>
+            <button className="filter cake" onClick={()=>productFiltering("cake")}><CakeIcon /></button>
+            <button className="filter chilled" onClick={()=>productFiltering("chilled")}><ChilledIcon /></button>
+            <button className="filter coffee" onClick={()=>productFiltering("coffee")}><CoffeeIcon /></button>
+            <button className="filter dairy" onClick={()=>productFiltering("dairy")}><DairyIcon /></button>
+            <button className="filter misc" onClick={()=>productFiltering("misc")}><MiscIcon /></button>
+            <button className="filter pastry" onClick={()=>productFiltering("pastry")}><PastryIcon /></button>
+            <button className="filter powder" onClick={()=>productFiltering("powder")}><PowderIcon /></button>
+            <button className="filter sauce" onClick={()=>productFiltering("sauce")}><SauceIcon /></button>
+            <button className="filter syrup" onClick={()=>productFiltering("syrup")}><SyrupIcon /></button>
+            <button className="filter tea" onClick={()=>productFiltering("tea")}><TeaIcon /></button>
         </div>
         <div id="productScroll">
             {productAccordion}
